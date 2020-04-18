46dbac9e5862f6c245c4d60f7a07cf6613602e49
alexandrarybalko <alexandrarybalko21@gmail.com>
2020-04-18 18:17:06 +0600

    6th commit

--- QuadraticEquation/src/QuadraticEquation.java
+++ QuadraticEquation/src/QuadraticEquation.java
@@ -1,24 +1,36 @@
-import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.Random;
 
 public class QuadraticEquation {
-    public static void main(String[] args) {
-        System.out.println("Введите коффициенты a, b, с квадратного уравнения");
-        Scanner in = new Scanner(System.in);
-        double a = in.nextInt(), b = in.nextInt(), c = in.nextInt();
-        Solver equation = new Solver(a, b, c);
-        switch (equation.count) {
-            case 0:
-                System.out.println("Корней нет");
-                break;
-            case 1:
-                System.out.println("Один корень - " + equation.x1);
-                break;
-            case 2:
-                System.out.println("Два корня - " + equation.x1 + " " + equation.x2);
-                break;
-            default:
-                System.out.println("Корень - любое вещественное число");
-                break;
+    public static void main(String[] args) throws Exception {
+        int i = 0;
+        ExecutorService executor = Executors.newFixedThreadPool(100);
+        for (; i < 100; ++i) {
+            Solver equation = executor.submit(new MyCallable()).get();
+            switch (equation.count) {
+                case 0:
+                    System.out.println("Корней нет");
+                    break;
+                case 1:
+                    System.out.println("Один корень - " + equation.x1);
+                    break;
+                case 2:
+                    System.out.println("Два корня - " + equation.x1 + " " + equation.x2);
+                    break;
+                default:
+                    System.out.println("Корень - любое вещественное число");
+                    break;
+            }
+        }
+        executor.shutdown();
+    }
+
+    static class MyCallable implements Callable<Solver> {
+        @Override
+        public Solver call() {
+            Random random = new Random();
+            Solver equation = new Solver(random.nextInt(), random.nextInt(), random.nextInt());
+            return equation;
         }
     }
 }
